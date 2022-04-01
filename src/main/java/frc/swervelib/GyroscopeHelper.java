16ca39ef9fce730938201f44bc81cae462ba294f
Alberto Hernandez <albertohdzr98@gmail.com>
2022-04-01 00:53:05 -0600

    Pigeon2

--- src/main/java/frc/swervelib/GyroscopeHelper.java
+++ src/main/java/frc/swervelib/GyroscopeHelper.java
@@ -1,6 +1,7 @@
 package frc.swervelib;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
 import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 import com.kauailabs.navx.frc.AHRS;
 import com.kauailabs.navx.frc.AHRS.SerialDataType;
@@ -21,6 +22,11 @@ public final class GyroscopeHelper {
 
     public static Gyroscope createPigeonCAN(Integer id) {
         WPI_PigeonIMU pigeon = new WPI_PigeonIMU(id);
+        return new PigeonFactoryBuilder().build(pigeon);
+    }
+
+    public static Gyroscope createPigeon2(Integer id){
+        WPI_Pigeon2 pigeon = new WPI_Pigeon2(id);
         return new PigeonFactoryBuilder().build(pigeon);
     }
 
